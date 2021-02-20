8cebaadade6b44f816a11dfda11b0037d3d9b50e
ValentinVPK <valentinvpk@abv.bg>
2021-02-20 21:21:16 +0200

    Added classes exercises.

---
+++ C#-Advanced/DefiningClassesExercise/9.PokemonTrainer/Trainer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DefiningClasses
+{
+    public class Trainer
+    {
+        public Trainer(string name, Pokemon pokemon)
+        {
+            this.Name = name;
+            this.Badges = 0;
+            Collection = new List<Pokemon>();
+            Collection.Add(pokemon);
+        }
+        public string Name { get; set; }
+        public int Badges { get; set; }
+        public List<Pokemon> Collection { get; set; }
+
+    }
+}
---
+++ C#-Advanced/DefiningClassesExercise/ExercisesWithPersonClass(1-4)/Family.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace DefiningClasses
+{
+    public class Family
+    {
+        public List<Person> Members { get; set; }
+
+        public Family()
+        {
+            Members = new List<Person>();
+        }
+        public void AddMember(Person member)
+        {
+            Members.Add(member);
+        }
+
+        public Person GetOldestMember()
+        {
+           return Members.FirstOrDefault(x => x.Age == Members.Max(x => x.Age));
+        }
+    }
+}
