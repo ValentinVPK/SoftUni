8d463f8f2047b712f6b342264f6a3cb6b5cd8054
ValentinVPK <valentinvpk@abv.bg>
2021-02-23 21:33:35 +0200

    Added C# OOP inheritance.

---
+++ C#OOP/InheritanceLab/2.Multiple_Inheritance/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Farm
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            Puppy pup = new Puppy();
+            pup.Eat();
+            pup.Bark();
+            pup.Weep();
+        }
+    }
+}
---
+++ C#OOP/InheritanceLab/3.Hierarchical_Inheritance/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Farm
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            Cat toncho = new Cat();
+            toncho.Eat();
+            toncho.Meow();
+        }
+    }
+}
