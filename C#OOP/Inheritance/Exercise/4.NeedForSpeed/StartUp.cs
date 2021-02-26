ea2d9008ca58e27859f7d26f2ee0455813f93822
ValentinVPK <valentinvpk@abv.bg>
2021-02-26 16:13:28 +0200

    Added inheritance exercises.

---
+++ C#OOP/Inheritance/Exercise/4.NeedForSpeed/StartUp.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace NeedForSpeed
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            Car fiat = new Car(60, 300);
+
+            Console.WriteLine(fiat.DefaultFuelConsumption);
+        }
+    }
+}
---
+++ C#OOP/Inheritance/Exercise/6.Animals/StartUp.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Animals
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            while (true)
+            {
+                string line1 = Console.ReadLine();
+
+                if (line1 == "Beast!") break;
+
+                string[] data = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                string name = data[0];
+                int age = int.Parse(data[1]);
+                string gender = data[2];
+
+                if(string.IsNullOrEmpty(name) || age < 0 || string.IsNullOrEmpty(gender))
+                {
+                    Console.WriteLine("Invalid input!");
+                    continue;
+                }
+
+                if(line1 == "Cat")
+                {
+                    Cat cat = new Cat(name, age, gender);
+                    Console.WriteLine(cat);
+                    Console.WriteLine(cat.ProduceSound());
+                }
+                else if(line1 == "Dog")
+                {
+                    Dog dog = new Dog(name, age, gender);
+                    Console.WriteLine(dog);
+                    Console.WriteLine(dog.ProduceSound());
+                }
+                else if (line1 == "Frog")
+                {
+                    Frog frog = new Frog(name, age, gender);
+                    Console.WriteLine(frog);
+                    Console.WriteLine(frog.ProduceSound());
+                }
+                else if (line1 == "Tomcat")
+                {
+                    Tomcat tomcat = new Tomcat(name, age);
+                    Console.WriteLine(tomcat);
+                    Console.WriteLine(tomcat.ProduceSound());
+                }
+                else if (line1 == "Kitten")
+                {
+                    Kitten kitten = new Kitten(name, age);
+                    Console.WriteLine(kitten);
+                    Console.WriteLine(kitten.ProduceSound());
+                }
+            }
+        }
+    }
+}
