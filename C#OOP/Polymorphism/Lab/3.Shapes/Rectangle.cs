6f778a376e485c1acfe80225b08c2aedb9070e35
ValentinVPK <valentinvpk@abv.bg>
2021-03-07 09:33:33 +0200

    Added polymorphism lab tasks.

---
+++ C#OOP/Polymorphism/Lab/3.Shapes/Rectangle.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shapes
+{
+    public class Rectangle : Shape
+    {
+        private double height;
+        private double width;
+
+        public Rectangle(double height, double width)
+        {
+            this.Height = height;
+            this.Width = width;
+        }
+
+        public double Height
+        {
+            get => this.height;
+            private set
+            {
+                Validator.ThrowIfInvalidValue(value, "Invalid height");
+
+                this.height = value;
+            }
+        }
+
+        public double Width
+        {
+            get => this.width;
+            private set
+            {
+                Validator.ThrowIfInvalidValue(value, "Invalid width");
+                this.width = value;
+            }
+        }
+        public override double CalculateArea()
+        {
+            return this.Width * this.Height;
+        }
+
+        public override double CalculatePerimeter()
+        {
+            return 2 * this.Height + 2 * this.Width;
+        }
+
+        public override string Draw()
+        {
+            return "Drawing rectangle.";
+        }
+    }
+}
---
+++ C#OOP/Polymorphism/Lab/3.Shapes/Validator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shapes
+{
+    public static class Validator
+    {
+        public static void ThrowIfInvalidValue(double value, string message)
+        {
+            if(value <= 0)
+            {
+                throw new ArgumentException(message);
+            }
+        }
+    }
+}
