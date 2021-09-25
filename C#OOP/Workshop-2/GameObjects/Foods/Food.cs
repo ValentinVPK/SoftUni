ada19b9bbb57fef1da5c872b9ce950951a1fe4ca
ValentinVPK <valentinvpk@abv.bg>
2021-09-25 11:54:00 +0300

    Added workshop 2 to C# OOP

---
+++ C#OOP/Workshop-2/GameObjects/Foods/Food.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SimpleSnake.GameObjects.Foods
+{
+    public abstract class Food : Point
+    {
+        private char foodSymbol;
+        private readonly Random random;
+        private readonly Wall wallInfo;
+        private ConsoleColor foodColor;
+        protected Food(Wall wall, char foodSymbol, int foodPoints, ConsoleColor foodColor) 
+            : base(wall.LeftX, wall.TopY)
+        {
+            this.random = new Random();
+            this.wallInfo = wall;
+            this.foodSymbol = foodSymbol;
+            this.FoodPoints = foodPoints;
+            this.foodColor = foodColor;
+        }
+
+        public int FoodPoints { get; private set; }
+
+        public void SetRandomPosition(Queue<Point> snakeElement)
+        {
+            this.LeftX = random.Next(2, wallInfo.LeftX - 2);
+            this.TopY = random.Next(2, wallInfo.TopY - 2);
+
+            bool isPointOfSnake = snakeElement.Any(p => p.LeftX == this.LeftX && p.TopY == this.TopY);
+
+            while(isPointOfSnake)
+            {
+                this.LeftX = random.Next(2, wallInfo.LeftX - 2);
+                this.TopY = random.Next(2, wallInfo.TopY - 2);
+
+                isPointOfSnake = snakeElement.Any(p => p.LeftX == this.LeftX && p.TopY == this.TopY);
+            }
+
+            Console.BackgroundColor = this.foodColor;
+            this.Draw(foodSymbol);
+            Console.BackgroundColor = ConsoleColor.White;
+        }
+
+        public bool IsFoodPoint(Point snakeHead)
+        {
+            return this.LeftX == snakeHead.LeftX && this.TopY == snakeHead.TopY;
+        }
+    }
+}
---
+++ C#OOP/Workshop-2/GameObjects/Foods/FoodHash.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimpleSnake.GameObjects.Foods
+{
+    public class FoodHash : Food
+    {
+        private const char FoodSymbol = '#';
+        private const int Points = 3;
+        private const ConsoleColor Color = ConsoleColor.Blue;
+        public FoodHash(Wall wall) 
+            : base(wall, FoodSymbol, Points, Color)
+        {
+        }
+    }
+}
