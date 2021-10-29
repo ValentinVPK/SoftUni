e4c3b78b09ca3c085251c01c15a3ceebbb22540d
ValentinVPK <valentinvpk@abv.bg>
2021-10-29 09:20:50 +0300

    Added Entity Relations to C# Entitity Framework Core

---
+++ EntityFrameworkCore/04.EntityRelations/FootballBetting.Models/Bet.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P03_FootballBetting.Data.Models
+{
+    public class Bet
+    {
+
+        //•	Bet – BetId, Amount, Prediction, DateTime, UserId, GameId
+
+        public int BetId { get; set; }
+
+        public decimal Amount { get; set; }
+
+        //possible change
+        public double Prediction { get; set; }
+
+        public DateTime DateTime { get; set; }
+
+        public int UserId { get; set; }
+        public User User { get; set; }
+
+        public int GameId { get; set; }
+        public Game Game { get; set; }
+    }
+}
